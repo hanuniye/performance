f0898ecefdaf25f3448483f6e6a1f5b68a004d0b
hanuniye <hanuniye@gmail.com>
2024-12-23 23:55:44 +0300

    updated

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -3,14 +3,14 @@ import axios from "axios";
 const BASE_URL = "https://performance-review.onrender.com/api";
 
 export default axios.create({
-  baseURL: BASE_URL,
-});
+    baseURL: BASE_URL
+})
 
 //this private axios that we are working with JSON web tokens
 export const privateAxios = axios.create({
-  baseURL: BASE_URL,
-  headers: {
-    "Content-Type": "application/json",
-  },
-  withCredentials: true,
-});
+    baseURL: BASE_URL,
+    headers: {
+        "Content-Type": "application/json"
+    },
+    withCredentials: true,
+})
