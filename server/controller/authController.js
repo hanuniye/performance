80246f29ce1e9a34c0d7d07d9302f6580352331a
hanuniye <hanuniye@gmail.com>
2024-12-24 11:18:52 +0300

    updating access token

--- server/controller/authController.js
+++ server/controller/authController.js
@@ -30,7 +30,7 @@ export const login = async (req, res, next) => {
     const accessToken = jwt.sign(
       { email: validEmail.email, role: validEmail.role },
       process.env.ACCESS_TOKEN_SECRET,
-      { expiresIn: "15s" }
+      { expiresIn: "15d" }
     );
 
     // const refreshToken = jwt.sign(
